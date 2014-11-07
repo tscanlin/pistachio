fa6f2962578cd96cb914d3678d21de159e444fcf
Jon San Miguel <sanmiguelje@yahoo.com>
2014-11-07 07:10:19 -0800

    Modularize code

---
+++ pistachio/s3.py
@@ -0,0 +1,51 @@
+import boto
+import os
+import yaml
+
+import settings
+import util
+
+# Create an S3 connection
+def connect(s):
+  key = s['key']
+  secret = s['secret']
+  return boto.connect_s3(key,secret)
+
+
+# Load and merge the yaml files from S3
+def load(s=settings.load()):
+  s = settings.validate(s)
+
+  # Load the file from a cache if one exists
+  if s['cache'] and os.path.isfile(s['cache']):
+    return yaml.load(open(s['cache'],'r'))
+
+  conn = connect(s)
+  bucket = conn.get_bucket(s['bucket'], validate=False)
+
+  # Initialize the config with the pistachio keys
+  config = {
+    'pistachio': {
+      'key': conn.access_key,
+      'secret': conn.secret_key,
+    }
+  }
+  # For each folder
+  for folder in reversed(s['path']):
+    # Iterate through yaml files in the set folder
+    for key in bucket.list(folder+'/', delimiter='/'):
+      if key.name.endswith('.yaml'):
+        try:
+          contents = key.get_contents_as_string()
+          # Update the config with the config partial
+          config_partial = yaml.load(contents)
+          util.merge_dicts(config, config_partial)
+        except boto.exception.S3ResponseError:
+          pass # Access denied. Skip this one.
+
+  # If s['cache'] is set, we should cache the config locally to a file
+  if s['cache']:
+    with open(s['cache'], 'w') as pistachio_cache:
+      pistachio_cache.write( yaml.dump(config, default_flow_style=False))
+
+  return config
---
+++ pistachio/settings.py
@@ -0,0 +1,58 @@
+import os
+import yaml
+
+# Load settings from a pistachio.yaml file
+def load():
+  settings = {}
+  settings_files = []
+
+  # Search bottom up from the current directory for a pistachio.yaml file
+  path = os.getcwd()
+  while True:
+    settings_file = os.path.join(path, 'pistachio.yaml')
+    if os.path.isfile(settings_file): settings_files.append(settings_file)
+    if path == '/': break
+    path = os.path.abspath(os.path.join(path, os.pardir))
+
+  # Check for a 'pistachio.yaml' file in the HOME directory
+  if os.getenv('HOME'):
+    settings_file = os.path.abspath(os.path.join(os.getenv('HOME'), 'pistachio.yaml'))
+    if os.path.isfile(settings_file): settings_files.append(settings_file)
+
+  # Load settings from files
+  for settings_file in reversed(settings_files):
+    loaded = yaml.load(open(settings_file,'r'))
+    # Expand the fullpath of the cache, if set
+    if 'cache' in loaded: loaded['cache'] = os.path.abspath(os.path.join(os.path.dirname(settings_file), loaded['cache']))
+    settings.update(loaded)
+
+  # Override settings from any environment variables
+  for var, val in os.environ.iteritems():
+    if var == 'PISTACHIO_PATH':
+      # When path is set through environment variables, folders are ':' delimited
+      settings['path'] = val.split(':')
+    elif var.startswith('PISTACHIO_'):
+      key = var.split('PISTACHIO_', 1)[1]
+      settings[key.lower()] = val
+
+  return settings
+
+
+# Validate settings and set defaults
+def validate(settings):
+  # Required keys
+  if 'cache' in settings and settings['cache'] and os.path.isfile(settings['cache']):
+    pass # Only 'cache' key is required, if the cache already exists.
+  else:
+    for required_key in ['key', 'secret', 'bucket']:
+      if required_key not in settings: raise Exception('The "%s" key is required.' % required_key)
+
+  # Default settings
+  if 'path' not in settings or settings['path'] is None: settings['path'] = ['']
+  if 'cache' not in settings: settings['cache'] = None
+
+  # Type conversions
+  if not isinstance(settings['path'], list):
+    settings['path'] = [settings['path']]
+
+  return settings
