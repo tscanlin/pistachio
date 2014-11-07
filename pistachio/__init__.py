66ed23208e34d6369157bd06a17af103d229be6a
Jon San Miguel <sanmiguelje@yahoo.com>
2014-11-06 23:29:46 -0800

    Alter pistachio file loading

--- pistachio/__init__.py
+++ pistachio/__init__.py
@@ -5,31 +5,43 @@ import yaml
 
 # Create an S3 connection
 def connect(settings):
-  # Use key and secret from pistachio.yaml if set
-  if settings['key'] and settings['secret']:
-    key = settings['key']
-    secret = settings['secret']
-  # Otherwise, get it from the environment
-  else:
-    key = os.getenv('AWS_ACCESS_KEY_ID')
-    secret = os.getenv('AWS_SECRET_ACCESS_KEY')
+  key = settings['key']
+  secret = settings['secret']
   return boto.connect_s3(key,secret)
 
 
 # Load settings from a pistachio.yaml file
 def load_settings():
-  path = os.getcwd()
+  settings = {}
+  settings_files = []
+
   # Search bottom up from the current directory for a pistachio.yaml file
+  path = os.getcwd()
   while True:
     settings_file = os.path.join(path, 'pistachio.yaml')
-    if os.path.isfile(settings_file):
-      loaded = yaml.load(open(settings_file,'r'))
-      # Expand the fullpath of the cache, if set
-      if 'cache' in loaded: loaded['cache'] = os.path.abspath(os.path.join(path, loaded['cache']))
-      return loaded
-    if path == '/': raise Exception('No pistachio.yaml file found')
+    if os.path.isfile(settings_file): settings_files.append(settings_file)
+    if path == '/': break
     path = os.path.abspath(os.path.join(path, os.pardir))
 
+  # Check for a 'pistachio.yaml' file in the HOME directory
+  if os.getenv('HOME'):
+    settings_file = os.path.abspath(os.path.join(os.getenv('HOME'), 'pistachio.yaml'))
+    if os.path.isfile(settings_file): settings_files.append(settings_file)
+
+  # Load settings from files
+  for settings_file in reversed(settings_files):
+    loaded = yaml.load(open(settings_file,'r'))
+    # Expand the fullpath of the cache, if set
+    if 'cache' in loaded: loaded['cache'] = os.path.abspath(os.path.join(os.path.dirname(settings_file), loaded['cache']))
+    settings.update(loaded)
+
+  # Override settings from any environment variables
+  for var, val in os.environ.iteritems():
+    if var.startswith('PISTACHIO_'):
+      key = var.split('PISTACHIO_', 1)[1]
+      settings[key.lower()] = val
+
+  return settings
 
 # Validate settings and set defaults
 def validate_settings(settings):
