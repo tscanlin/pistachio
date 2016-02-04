978ac08667c86eaa28f6233e1f6a996206da521f
Brian Lucas <brian.lucas@optimizely.com>
2016-02-04 11:09:37 -0800

    fix message

--- pistachio/s3.py
+++ pistachio/s3.py
@@ -82,8 +82,8 @@ def fetch_config_partial(folder, key):
     global config_partials
     config_partials[folder].append(yaml.load(contents))
 
-  except boto.exception.S3ResponseError:
-    print("boto exception on %s" % key)
+  except boto.exception.S3ResponseError as e:
+    print("s3 exception on %s: %s" % (key, e))
   except:
     print("Unexpected error: %s" % sys.exc_info()[0])
   finally:
